a5a5e76c8969d983aa15413794bf3ac77c9e62e4
kheldar-ai <kheldar@drasniano.com>
2021-06-01 12:18:18 +0200

    Ejercicio 3 curso Angular-jcyl-2021

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -9,7 +9,11 @@ export class SolicitudComponent implements OnInit {
 
   solicitud = {nombre: "Javier"}
 
-  constructor() { }
+  constructor() { 
+    setInterval(() => {
+      this.solicitud.nombre= '' + Math.random();
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
